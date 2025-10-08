23a78b7004860906e15aedfe7311769ec6c95ce3
Isabelle <isabelle.dahlstromm@gmail.com>
2025-10-08 11:24:03 +0200

    refactor: remove docstring comments from test methods in test_dice.py

--- test/test_dice.py
+++ test/test_dice.py
@@ -22,13 +22,12 @@ from src.dice import Dice
 class Test_Dice_Roll(unittest.TestCase):
 
     def test_init_default_object(self):
-        """Instantiate an object and check its properties"""
         res = Dice()
         exp = Dice
         self.assertIsInstance(res, exp)
 
     def test_roll_dice(self):
-        """Test roll dice"""
+
         dice = Dice()
         res = dice.roll()
         exp = 1 <= res <= 6
